2bbd14aa75fad27c550d0049a2e6e3b85ce93801
Eric Bortoleto <ericbortoleto_9@outlook.com>
2026-03-17 14:07:13 -0300

    Add listCoroutine to PostService and update MainActivity to fetch data using Coroutines

--- app/src/main/java/com/example/retrofit/MainActivity.kt
+++ app/src/main/java/com/example/retrofit/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.retrofit
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 import retrofit2.Callback
 import retrofit2.Response
 
@@ -36,6 +39,14 @@ class MainActivity : AppCompatActivity() {
                 val s = ""
             }
         })
+
+        lifecycleScope.launch {
+            val result = postService.listCoroutine()
+            if (result.isSuccessful) {
+                val abc = result.body()
+                Log.d("TAG", "onCreate: $abc")
+            }
+        }
 
     }
 }
